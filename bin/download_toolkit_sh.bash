e45378dc777371e6bd4c9c9f6736f7af800704ce
AndF111111 <andiferdy639@gmail.com>
2023-12-01 02:48:12 +0800

    Delete bin/download_toolkit_sh.bash
    Delete

--- bin/download_toolkit_sh.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-readonly version="v1.0"
-
-git clone -b "$version" "https://github.com/jmatsu/github-actions-toolkit.git" github-actions-toolkit
-cp github-actions-toolkit/toolkit.sh ./toolkit.sh
-rm -fr github-actions-toolkit
